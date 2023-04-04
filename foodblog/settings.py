36c050fc29593cfab85e399df5322121473bfaf8
DaveGutHib <davidbsheehan@gmail.com>
2023-04-04 09:45:21 +0000

    set debug to false

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ["foodblog.herokuapp.com","localhost"]
 
